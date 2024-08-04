a27855ee1a21a53d4c81eb164bb96d3b7e4047ad
Lorenz815 <lorenzvillalobosmotorcentral@gmail.com>
2024-08-04 21:34:56 +0800

    Aug 4 last update UI responsive
    index.css
    styles.css
    index.php
    login.php
    time_record.php
    
    responsive index&login.php

--- login.php
+++ login.php
@@ -16,17 +16,53 @@ if (isset($_SESSION['student_id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include 'includes/top_include.php'; ?>
     <link rel="stylesheet" href="assets/css/styles.css">
-
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
 </head>
 
 <body>
+        <div class="col-lg-3">
+          <div class="card">
+            <div class="card-body">
+            <div class="login-container-header">
+                <img src="assets/img/pnc-logo.png" alt="University of Cabuyao Logo">
+                <span>&nbsp;Internship Management System</span>
+            </div><br>
+            <div class="card-title">
+              <h5>INTERN</h5>
+            </div>
+              <!-- Vertical Form -->
+              <form id="loginForm" class="row g-3">
+                <div class="col-12">
+                  <label for="inputNanme4" class="form-label">Student Id</label>
+                  <input type="text" class="form-control" id="student_id" name="student_id">
+                </div>
+                <div class="col-12">
+                  <label for="inputEmail4" class="form-label">Pin</label>
+                  <input type="password" class="form-control" id="pin" name="pin">
+                </div><br><br><br><br>
+                <div class="d-grid gap-2 mt-3">
+                    <button class="btn btn-success btn-lg" type="submit"><i class="bx bx-door-open"></i> Login</button>
+                </div>
+                <div class="text-center">
+                  <a class="backbtn" href="./">Back</a>
+                </div>
+              </form><!-- Vertical Form -->
+              <div id="message" class="message"></div>
+            </div>
+          </div>
+
+
+
+
+
+    <!-------- For edit
     <div class="login-container">
         <div class="login-container-header">
             <img src="assets/img/pnc-logo.png" alt="University of Cabuyao Logo">
             <span>&nbsp;Internship Management System</span>
         </div>
         <h1>INTERN</h1>
-        <form id="loginForm">
             <form id="loginForm">
                 <label for="student_id">Student ID:</label>
                 <input type="text" id="student_id" name="student_id" required><br>
@@ -40,7 +76,7 @@ if (isset($_SESSION['student_id'])) {
             </form>
             <div id="message" class="message"></div>
     </div>
-
+    -------->
     <script>
         document.getElementById('loginForm').addEventListener('submit', async (event) => {
             event.preventDefault();
